a38218865437923127936f4452dd408854f86e0a
Constantin Veziteu <c.veziteu@gmail.com>
2019-07-30 14:16:56 +0300

    Developer update #12 - 30.07.2019
    1) Small changes to profile page design
    2) Added email address validation + error messages

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,44 +1,45 @@
 import React from 'react';
-
+import './profile.css';
 class Profile extends React.Component {
      
     render() {
         const {userProfile} = this.props;
+        const joinedDate = Date(userProfile.joined.toString());
         return (
-            <div className="mw6 center pa3 pa4-ns mv3 bg-white-10 white">
+            <div className="mw7 center pa3 pa4-ns mv3 white">
                 <div className='tc'>
-                    <h1 className="f2 yellow">{userProfile.username}</h1>
+                    <h1 className="fw4"><span className='light-yellow'>{userProfile.username}</span>'s profile</h1>
                 </div>
-                <table className='w-100 pt4' cellPadding='1' cellSpacing='2'>
+                <table className='w-100 pt4' cellPadding='5' cellSpacing='0'>
                     <tbody>
-                        <tr className='mt3'>
-                            <td className='pa3 ba'>
+                        <tr className='mt3 trstyle'>
+                            <td className='pa3'>
                                 <strong>
                                     Email:
                                 </strong>
                             </td>
-                            <td className='light-yellow ba'>
+                            <td className='light-yellow'>
                                 {userProfile.email}
                             </td>
                         </tr>
                         <tr className='mt3'>
-                            <td className='pa3 ba'>
+                            <td className='pa3'>
                                 <strong>
                                 Entries:
                                 </strong>
                             </td>
-                            <td className='light-yellow ba'>
+                            <td className='light-yellow'>
                                 {userProfile.entries}
                             </td>
                         </tr>
                         <tr className='mt3'>
-                            <td className='pa3 ba'>
+                            <td className='pa3'>
                                 <strong>
                                     Joined:
                                 </strong>
                             </td>
-                            <td className='light-yellow ba'>
-                                {userProfile.joined}
+                            <td className='light-yellow'>
+                                {joinedDate}
                             </td>
                         </tr>
                     </tbody>
